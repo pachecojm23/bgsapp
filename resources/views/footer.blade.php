d6e43073e45c17633a5f81aade82bc92936242b3
JOSÃE MANUEL <pachecojm23@gmail.com>
2016-04-15 23:00:34 -0600

    Agregada dirección a Footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,5 +5,6 @@
 	</ul>
 	<ul class="copyright">
 		<li>&copy; Bill Gates School and American Eagles Academy. All rights reserved.</li>
+        <li>Barrio el Calvario, Av. Benjamín Estrada Valiente, #8, Metapán, Santa  Ana, El Salvador</li>
 	</ul>
 </footer>
